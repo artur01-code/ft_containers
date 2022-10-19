89b18bc493f8ad8cd265d80b51565b0b9de01240
jtomala <jtomala@students.42wolfsburg.de>
2022-10-19 14:14:59 +0200

    video finished

--- main.cpp
+++ main.cpp
@@ -9,32 +9,45 @@ void print_varray(ft::vector<int> &rhs)
 int main(void)
 {
 	//		THE ORIGINAL VECTOR CLASS
-	// {
-	// 	std::cout << std::string(60, '-') << std::endl;
-	// 	std::cout << "original vector" << std::endl;
-	// 	std::cout << std::string(60, '-') << std::endl;
-	// 	std::vector<int> v1;
-	// 	for (int i = 0; i < 10; i++)
-	// 		v1.push_back(i);
-	// 	for (std::vector<int>::iterator iter = v1.begin(); iter != v1.end(); iter++)
-	// 		std::cout << *iter << " ";
-	// 	std::cout << " | | " << std::endl;
-	// }
+	{
+		std::cout << std::string(60, '-') << std::endl;
+		std::cout << "original vector" << std::endl;
+		std::cout << std::string(60, '-') << std::endl;
+		std::vector<int> v1;
+		for (int i = 0; i < 10; i++)
+			v1.push_back(i);
+		for (std::vector<int>::iterator iter = v1.begin(); iter != v1.end(); iter++)
+			std::cout << *iter << " ";
+		std::cout << " | | " << std::endl;
+	}
 	//		TESTING MY OWN VECTOR CLASS
 	{
 		std::cout << std::string(60, '-') << std::endl;
 		std::cout << "my own vector" << std::endl;
 		std::cout << std::string(60, '-') << std::endl;
 		ft::vector<int> v1;
-		for (int i = 0; i < 5; i++)
+		for (int i = 0; i < 10; i++)
 			v1.push_back(i);
-		ft::vector<int> v2;
+		
+		// try
+		// {
+		// 	for (int i = 0; i < 12; i++)
+		// 		v1.pop_back();
+		// }
+		// catch(const std::exception& e)
+		// {
+		// 	std::cerr << e.what() << std::endl;
+		// }
+		
+		ft::vector<int> v2(1);
 		v2 = v1; // test the copy assigment operator
-		ft::vector<int> v3(v1); //test copy constructor
+		// ft::vector<int> v3(v1); //test copy constructor
+		v2.ersase(7);
+		v2.insert(7, 10);
 
 		print_varray(v1);
 		print_varray(v2);
-		print_varray(v3);
+		// print_varray(v3);
 
 	}
 }
--- vector.hpp
+++ vector.hpp
@@ -2,8 +2,9 @@
 # define VECTOR_HPP
 
 #include <iostream>
+#include <exception>
 
-//https://www.youtube.com/watch?v=YpNCBw-cPWw : 23:51
+//https://www.youtube.com/watch?v=YpNCBw-cPWw
 
 namespace ft {
 
@@ -17,36 +18,97 @@ class vector
 		}
 		vector(int elements, int value = 0) : _size(elements), _capacity(elements + 5), _array(new int[_capacity])
 		{
-			for (int i = 0; i < Size(); ++i)
+			for (int i = 0; i < size(); ++i)
 			{
 				_array[i] = value;
 			}
 		}
 		vector(const vector& rhs) : _size(rhs._size), _capacity(rhs._capacity), _array(new int[_capacity])
 		{
-			for (int i = 0; i < rhs.Size(); i++)
+			for (int i = 0; i < rhs.size(); ++i)
 			{
 				_array[i] = rhs._array[i];
 			}
 		}
 
-		// vector(const std::initilaizer_list<int>& list) : _size(0), _capacity(list.Size() + 5), _array(new int[_capacity])
+		// vector(const std::initilaizer_list<int>& list) : _size(0), _capacity(list.size() + 5), _array(new int[_capacity])
 		// {
-		// 	for (int i = 0; i < list.Size(); ++i)
+		// 	for (int i = 0; i < list.size(); ++i)
 		// 		push_back(i);
 		// }
 
 		vector& operator=(const vector &rhs)
 		{
-			delete[] _array;
-			this->_size = rhs._size;
-			this->_capacity = rhs._capacity;
-			this->_array = new int[_capacity];
-			for (int i = 0; i < rhs.Size(); ++i)
+			if (rhs._size > _size)
+			{
+				delete[] _array;
+				this->_capacity = rhs._capacity + 5;
+				this->_array = new int[_capacity];
+			}
+			for (int i = 0; i < rhs.size(); ++i)
 			{
 				_array[i] = rhs._array[i];
 			}
+			this->_size = rhs._size;
 			return (*this);
+		}
+
+		int& operator[](int index)
+		{
+			return (_array[index]);
+		}
+		int& front()
+		{
+			return (_array[0]);
+		}
+		int& back()
+		{
+			return (_array[_size - 1]);
+		}
+
+		void insert(int index, int value)
+		{
+			if ((index < 0) || (index >= _size))
+			{
+				std::cerr << "Insert - Index out of Range" << std::endl;
+				return ;
+				// throw (Exception);
+			}
+			if (_size != _capacity)
+			{
+				for (int i = _size - 1; i >= index; --i)
+					_array[i + 1] = _array[i];
+				_array[index] = value;
+				++_size;
+			}
+			else
+			{
+				_capacity *= 2;
+				int *newArray = new int[_capacity];
+				for (int i = 0; i < _size; ++i)
+					newArray[i] = _array[i];
+				delete[] _array;
+				_array = newArray;
+				insert(index, value);
+			}
+		}
+
+		void ersase(int index)
+		{
+			if ((index < 0) || (index >= _size))
+			{
+				std::cerr << "Erase - Index out of Range" << std::endl;
+				return ;
+				// throw (Exception);
+			}
+			for (int i = index; i < _size - 1; ++i)
+				_array[i] = _array[i + 1];
+			--_size;
+		}
+
+		void clear(void)
+		{
+			_size = 0;
 		}
 
 		~vector(void)
@@ -65,8 +127,6 @@ class vector
 	public:
 		void push_back(int value)
 		{ 
-			// TODO: what if filled?
-			// std::cout << "push_back(" << value << ")" << std::endl;
 			if (_size < _capacity)
 			{
 				_array[_size] = value;
@@ -76,29 +136,46 @@ class vector
 			{
 				_capacity *= 2;
 				int *newArray = new int[_capacity];
+				for (int i = 0; i < _size; ++i)
+					newArray[i] = _array[i];
+				_array[_size] = value;
+				_size++;
+				delete [] _array;
+				_array = newArray;
 			}
 		}
 
-		bool Empty(void) const
+		void pop_back(void)
+		{
+			if (_size == 0)
+			{
+				std::cerr << "pop back on empty vector" << std::endl;
+				return ;
+				// throw PoppingBackEmptyVector();
+			}
+			--_size;
+		}
+
+		bool empty(void) const
 		{
 			return (_size == 0);
 		}
 
-		int Size(void) const
+		int size(void) const
 		{
 			return (_size);
 		}
 
-		int Capacity(void) const
+		int capacity(void) const
 		{
 			return (_capacity);
 		}
 
 		bool operator==(const vector& rhs) const
 		{
-			if (Size() != rhs.Size())
+			if (size() != rhs.size())
 				return (false);
-			for (int i = 0; i < Size(); ++i)
+			for (int i = 0; i < size(); ++i)
 				if (_array[i] != rhs._array[i])
 					return (false);
 			return (true);
@@ -108,6 +185,15 @@ class vector
 		{
 			return !(*this == rhs);
 		}
+
+		class PoppingBackEmptyVector : std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("Pop back an empty vector!");
+				}
+		};
 
 		friend std::ostream& operator<<(std::ostream& ostr, const vector& rhs)
 		{
